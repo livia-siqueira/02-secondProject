8d60e4cc44ef8db2c4983208674770ebde16a5b0
Lívia Siqueira <livia.siqueira12@outlook.com>
2021-10-04 15:58:47 -0300

    commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="your money" />
+        <img src={logoImg} alt="dtMoney" />
         <button type="button">Nova Transação</button>
       </Content>
     </Container>
